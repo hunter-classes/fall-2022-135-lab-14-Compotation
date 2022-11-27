2e266311d2ac13f4072ac7caaa56d3fa5f596487
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-26 21:58:53 -0500

    decrease push back iterations

--- tests.cpp
+++ tests.cpp
@@ -6,9 +6,9 @@
 
 TEST_CASE("pushing back") {
   auto vec = MyVector<std::string>();
-  for (int i = 0; i < 10000; i++) {
+  for (int i = 0; i < 50; i++) {
     std::string element = "hie" + std::to_string(i * 50);
     vec.push_back(element);
-    CHECK_EQ(vec[i], "hie" + std::to_string(i * 50));
+    CHECK_EQ(vec[i], element);
   }
 }
